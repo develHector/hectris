a853a7d92c9af11c5fac71ce1f2ea0b8d358f0d9
develHector <devel.hector@hotmail.com>
2020-04-18 11:02:07 -0500

    Added html entities instead of ionic objects and added comments and extra files

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # hectris
+Tetris as a PWA (Progressive Web Application)
+Developed under the guidelines of the great Gary Simon under https://youtu.be/Xm_EZAGGGdU
+March 2020
+
+# code
+You can find the specifics under /srs/app/home
+
+# intentionally untracked files 
+
+*~
+*.sw[mnpcod]
+.tmp
+*.tmp
+*.tmp.*
+*.sublime-project
+*.sublime-workspace
+.DS_Store
+Thumbs.db
+UserInterfaceState.xcuserstate
+$RECYCLE.BIN/
+
+*.log
+log.txt
+npm-debug.log*
+
+/.idea
+/.ionic
+/.sass-cache
+/.sourcemaps
+/.versions
+/.vscode
+/coverage
+/dist
+/node_modules
+/platforms
+/plugins
+/www
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,11 @@ import { Component, DebugElement } from '@angular/core';
 import { debug } from 'util';
 import { SelectMultipleControlValueAccessor } from '@angular/forms';
 
-export class Celda {
+export class Cell {
   background_color: string;
   border_color: string;
   constructor(background_color: string, border_color: string) {
-    let self: Celda = this;
+    let self: Cell = this;
     self.background_color = background_color;
     self.border_color = border_color;
   }
@@ -22,7 +22,7 @@ export class Punto {
   }
 }
 
-export class Figura {
+export class Block {
   x: number;
   y: number;
   base: Array<Punto>;
@@ -36,18 +36,18 @@ export class Figura {
 })
 export class HomePage {
 
-  cells: Celda[][] = [];
+  cells: Cell[][] = [];
   timer: any = null;
   Score: number = 0;
   HiScore: number = 0;
-  Velocidad: number = 1601; // 50 niveles+1ms
+  Speed: number = 1601; // 50 levels + 1 ms
 
   static MAX: number = 20;
   static MAY: number = 10;
 
-  CeldaDefault: Celda = { background_color: 'black', border_color: 'rgb(1, 1, 1)' };
+  DefaultCell: Cell = { background_color: 'black', border_color: 'rgb(1, 1, 1)' };
 
-  figuras: Figura[] = [
+  blocks: Block[] = [
     { x: 0, y: 4, base: [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 }], color: 'crimson' }, // o
     { x: 0, y: 4, base: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 2, y: 0 }], color: 'blue' }, // J
     { x: 0, y: 4, base: [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 2, y: 1 }], color: 'darkorange' }, // L
@@ -57,60 +57,56 @@ export class HomePage {
     { x: 0, y: 4, base: [{ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 1, y: 2 }], color: 'cyan' }, // T
   ];
 
-  activa: Figura = { x: 0, y: 0, base: [], color: "" };
+  active: Block = { x: 0, y: 0, base: [], color: "" };
 
   constructor() {
     let self: HomePage = this;
-    self.InitBoard();    
-  }
-
-  OnStartStop() {
-    let self: HomePage = this;
     self.InitBoard();
-    self.ObtenerNuevaFigura();
-    self.timer = setInterval(() => self.Gravity(), this.Velocidad);
   }
 
-  // inicializamos en blanco el board completo
+
+
+  // initialize the whole X*Y board
   InitBoard() {
     let self: HomePage = this;
 
     for (let i = 0; i < HomePage.MAX; i++) {
       self.cells[i] = [];
       for (let j = 0; j < HomePage.MAY; j++) {
-        self.cells[i][j] = new Celda(self.CeldaDefault.background_color, self.CeldaDefault.border_color);
+        self.cells[i][j] = new Cell(self.DefaultCell.background_color, self.DefaultCell.border_color);
       }
     }
     this.HiScore = parseInt(window.localStorage.getItem("Hectis.HiScore") || "0");
   }
 
-  // rutina que dibuja figura al azar al principio
-  ObtenerNuevaFigura() {
+  // present a radngm figure
+  GetNewFigure() {
     let self: HomePage = this;
 
-    // hacemos porque no se repita la misma que la anterior    
-    let cuenta = 0;
-    let nueva: Figura;
+    // do not repeat the previous figure
+    let count = 0;
+    let figure: Block;
     do {
-      let rnd = Math.floor(Math.random() * Math.floor(self.figuras.length));
-      nueva = self.figuras[rnd];
-    } while (nueva.color === self.activa.color && ++cuenta < 3);
+      let rnd = Math.floor(Math.random() * Math.floor(self.blocks.length));
+      figure = self.blocks[rnd];
+    } while (figure.color === self.active.color && ++count < 3);
 
-    // vemos si no se topó el board
-    for (let i of nueva.base) {
-      if (self.cells[i.x + nueva.x][i.y + nueva.y].background_color !== self.CeldaDefault.background_color) {
+    // check if board is overloaded to end the game
+    for (let i of figure.base) {
+      if (self.cells[i.x + figure.x][i.y + figure.y].background_color !== self.DefaultCell.background_color) {
         clearInterval(self.timer);
         self.timer = null;
         return;
       }
     }
 
-    // no solo metemos los puntos en el resultado, sino que los pintamos    
-    self.activa = { x: nueva.x, y: nueva.y, base: nueva.base.map(i => new Punto(i)), color: nueva.color } as Figura;
-    self.Move(false, i => nueva.base);
+    // set and move down
+    self.active = { x: figure.x, y: figure.y, base: figure.base.map(i => new Punto(i)), color: figure.color } as Block;
+    self.Move(false, i => figure.base);
 
   }
 
+  // remove completed rows and make upper ones fall down
   RemoveCompleteRows(): boolean {
     let self: HomePage = this;
 
@@ -118,7 +114,7 @@ export class HomePage {
     for (let i = 0; i < HomePage.MAX; i++) {
       let CompleteCells = 0;
       for (let j = 0; j < HomePage.MAY; j++) {
-        if (self.cells[i][j].background_color !== self.CeldaDefault.background_color) {
+        if (self.cells[i][j].background_color !== self.DefaultCell.background_color) {
           CompleteCells++;
         }
       }
@@ -127,50 +123,115 @@ export class HomePage {
 
     if (CompleteRows.length) {
 
-      // resaltamos un momento lo que va a eliminarse
+      // quickly "blink" what is going to be removed
       for (let i of CompleteRows) {
         for (let j = 0; j < HomePage.MAY; j++) {
-          self.cells[i][j] = new Celda('white', 'white');
+          self.cells[i][j] = new Cell('white', 'white');
         }
       }
 
-      // registramos el score exponencial por líneas
+      // register exponential-by-rows score 
       this.Score += (Math.pow(CompleteRows.length, 2) * 4);
 
-      // incrementamos la velocidad por evento (con independencia al número de líneas, para fomentar las mas posibles por evento)
-      this.Velocidad -= 32;
+      // increase the speed a little bit
+      this.Speed -= 32;
       if (this.Score > this.HiScore) {
         this.HiScore = this.Score;
         window.localStorage.setItem("Hectis.HiScore", this.HiScore.toString());
       }
 
-      // hacemos bajar todo lo de arriba
+      // a moment from now, make the rest fall down (this is to allow the blink to be visible without sleeps)
       setTimeout(() => {
         for (let c of CompleteRows) {
           for (let i = c; i > 0; i--) {
             for (let j = 0; j < HomePage.MAY; j++) {
               let upperCell = self.cells[i - 1][j];
-              self.cells[i][j] = new Celda(upperCell.background_color, upperCell.border_color);
+              self.cells[i][j] = new Cell(upperCell.background_color, upperCell.border_color);
             }
           }
         }
-        self.ObtenerNuevaFigura();
-      }, this.Velocidad/10);
+        self.GetNewFigure();
+      }, this.Speed / 10);
       return true;
     }
 
     return false;
   }
 
+  // Einstein showed Newton was wrong about gravity
+  Gravity() {
+    let self: HomePage = this;
+    let fin = self.Move(true, i => i.x++);
+    if (fin) {
+      if (!self.RemoveCompleteRows()) {
+        self.GetNewFigure();
+      }
+    }
+  }
 
+  // base method to move the active figure across the board on "lambda-movement" with collission checking
+  // note: it would be much more nice if it worked on a per-difference or incremental mode moving/cecking only what needed and not the whole figure, but comon guys, a day has only 24 hours
+  Move(gravity: boolean, lambda: (x: Block) => void): boolean {
+    let self: HomePage = this;
+
+    // generate a copy of the block place that copy on the intended position using the lambda instruction
+    let copy: Block = { x: self.active.x, y: self.active.y, base: self.active.base.map(i => new Punto(i)), color: self.active.color };    
+    lambda(copy);
+
+    // make invisible current block to avoid self collissions
+    for (let i of self.active.base) {
+      self.cells[i.x + self.active.x][i.y + self.active.y] = new Cell(self.DefaultCell.background_color, self.DefaultCell.border_color);
+    }
+
+    // check if the copied block is got to the bottom or collides with something
+    let bottom: boolean = false; let collision: boolean = false;
+    for (let i of copy.base) {
+      let x = i.x + copy.x;
+      let y = i.y + copy.y;
+      if (x >= HomePage.MAX || y < 0 || y >= HomePage.MAY || self.cells[x][y].background_color !== self.DefaultCell.background_color) {
+        if (gravity) {
+          bottom = true;
+          break;
+        } else {
+          collision = true;
+          break;
+        }
+      }
+    }
+
+    // if all clear convert the copied block in the active one
+    if (!bottom && !collision) {
+      self.active.x = copy.x;
+      self.active.y = copy.y;
+      self.active.base = copy.base.map(i => new Punto(i))
+    }
+
+    // make active block visible
+    for (let i of self.active.base) {
+      self.cells[i.x + self.active.x][i.y + self.active.y] = new Cell(self.active.color, self.active.color);
+    }
+
+    // if block hits bottom create a new figure
+    return bottom;
+  }
+
+  // button event (click)
+  OnStartStop() {
+    let self: HomePage = this;
+    self.InitBoard();
+    self.GetNewFigure();
+    self.timer = setInterval(() => self.Gravity(), this.Speed);
+  }
+
+  // button event (click)
   Rotate() {
     let self: HomePage = this;
 
-    // algoritmo de giro 90° de matriz
+    // 90° rotate algorithm (simple matrix algebra)
     self.Move(false, i => {
       let maxx = Math.max.apply(null, i.base.map(i => i.x));
       let maxy = Math.max.apply(null, i.base.map(i => i.y));
-      let max = Math.max( maxx, maxy) ;
+      let max = Math.max(maxx, maxy);
       i.base.forEach(j => {
         let prevy = j.y;
         j.y = max - j.x;
@@ -180,72 +241,29 @@ export class HomePage {
 
   }
 
-  Gravity() {
-    let self: HomePage = this;
-    let fin = self.Move(true, i => i.x++);
-    if (fin) {
-      if (!self.RemoveCompleteRows()) {
-        self.ObtenerNuevaFigura();
-      }
-    }
-  }
-
+  // button event (click)
   MoveDown() {
     let self: HomePage = this;
     while (!self.Move(true, i => i.x++)) { }
     clearInterval(self.timer);
     self.timer = null;
     self.RemoveCompleteRows();
-    self.timer = setInterval(() => self.Gravity(), this.Velocidad);
+    self.timer = setInterval(() => self.Gravity(), this.Speed);
   }
 
+  // button event (click)
   MoveLeft() {
     let self: HomePage = this;
     self.Move(false, i => i.y--);
   }
 
+  // button event (click)
   MoveRight() {
     let self: HomePage = this;
     self.Move(false, i => i.y++);
   }
 
-  Move(gravity: boolean, operator: (x: Figura) => void): boolean {
-    let self: HomePage = this;
 
-    let copia: Figura = { x: self.activa.x, y: self.activa.y, base: self.activa.base.map(i => new Punto(i)), color: self.activa.color };
-    operator(copia);
 
-    for (let i of self.activa.base) {
-      self.cells[i.x + self.activa.x][i.y + self.activa.y] = new Celda(self.CeldaDefault.background_color, self.CeldaDefault.border_color);
-    }
-
-    let fin: boolean = false;
-    let valid: boolean = true;
-    for (let i of copia.base) {
-      let x = i.x + copia.x;
-      let y = i.y + copia.y;
-      if (x >= HomePage.MAX || y < 0 || y >= HomePage.MAY || self.cells[x][y].background_color !== self.CeldaDefault.background_color) {
-        if (gravity) {
-          fin = true;
-          break;
-        } else {
-          valid = false;
-          break;
-        }
-      }
-    }
-
-    if (!fin && valid) {
-      self.activa.x = copia.x;
-      self.activa.y = copia.y;
-      self.activa.base = copia.base.map(i => new Punto(i))
-    }
-
-    for (let i of self.activa.base) {
-      self.cells[i.x + self.activa.x][i.y + self.activa.y] = new Celda(self.activa.color, self.activa.color);
-    }
-
-    return fin;
-  }
 
 }
